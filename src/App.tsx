8e7523d9793b0161a359ba7ef1171592a866897b
Geoffrey Irons <sdfgeoff@gmail.com>
2024-10-31 00:19:35 +1300

    Added game mode that scrambles the EQ for you to fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,49 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, useMemo } from 'react';
 import EQControl from './components/EQControl';
 import { EQBand, MixerSettings } from './components/MixerModel';
 import EQView from './components/EQView';
 
+
+
+const createEqFilterChain = (audioContext: AudioContext, length: number) => {
+  const filters = Array.from({ length: length }, () => {
+    const filter = audioContext.createBiquadFilter();
+    filter.type = 'peaking';
+    filter.frequency.value = 100;
+    filter.Q.value = 1;
+    filter.gain.value = 0;
+    return filter;
+  });
+
+  // Chain Filters
+  filters.forEach((filter, idx) => {
+    if (idx === 0) {
+      return
+    } else {
+      filters[idx - 1].connect(filter);
+    }
+  })
+  return filters
+}
+
+
+const useSourceNode = (audioElement: HTMLAudioElement | undefined, audioContext: AudioContext | undefined) => {
+  // You can only get one source node from an element, and sometimes useEffects fire multiple times. This
+  // creates a single source node for the provided element/context using a ref to ensure it happens only once,
+  // but externally acts like a useState so you can respond to changes
+  const sourceRef = useRef<MediaElementAudioSourceNode>();
+  const [source, setSource] = useState<MediaElementAudioSourceNode>();
+
+  useEffect(() => {
+    if (!sourceRef.current && audioElement && audioContext) {
+      const source = audioContext.createMediaElementSource(audioElement);
+      sourceRef.current = source;
+      setSource(source)
+    }
+  });
+
+  return source;
+}
 
 
 // Define a list of pre-existing audio tracks
@@ -20,11 +61,24 @@ function App() {
       { gain: 0, frequency: 8000, q: 1, name: 'HF' },
     ]
   });
-  const audioContextRef = useRef<AudioContext | null>(null);
 
-  const [audioElement, setAudioElement] = useState<HTMLAudioElement | null>(null);
-  const sourceRef = useRef<MediaElementAudioSourceNode | null>(null);
-  const [eqFilters, setEqFilters] = useState<BiquadFilterNode[] | undefined>();
+  const [audioContext, setAudioContext] = useState<AudioContext>();
+  const [audioElement, setAudioElement] = useState<HTMLAudioElement>();
+  const source = useSourceNode(audioElement, audioContext);
+
+  const userEqFilters = useMemo(() => {
+    if (!audioContext) {
+      return undefined
+    }
+    return createEqFilterChain(audioContext, mixerSettings.parametricEq.length);
+  }, [audioContext, mixerSettings.parametricEq.length]);
+
+  const hiddenEqFilters = useMemo(() => {
+    if (!audioContext) {
+      return undefined
+    }
+    return createEqFilterChain(audioContext, mixerSettings.parametricEq.length);
+  }, [audioContext, mixerSettings.parametricEq.length]);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -40,35 +94,32 @@ function App() {
   };
 
   const initAudioContext = () => {
-    if (!audioContextRef.current && audioElement) {
-      audioContextRef.current = new (window.AudioContext || window.webkitAudioContext)();
-      
-      const source = audioContextRef.current.createMediaElementSource(audioElement);
-      sourceRef.current = source;
+    // Have to wait for user interactino with page before this can be done
+    setAudioContext((old) => old ?? new AudioContext());
+  }
 
-      // Set up 4 EQ filters
-      const filters = mixerSettings.parametricEq.map(_ => {
-        const filter = audioContextRef.current!.createBiquadFilter();
-        filter.type = 'peaking';
-        filter.frequency.value = 100;
-        filter.Q.value = 1;
-        filter.gain.value = 0;
-        return filter;
-      });
-      setEqFilters(filters);
 
-      // Connect filters in series and to destination
-      filters.forEach((filter, idx) => {
-        if (idx === 0) {
-          source.connect(filter);
-        } else {
-          filters[idx - 1].connect(filter);
-        }
-      });
+  // Connect source -> hidden filters -> main filters -> destination
+  useEffect(() => {
+    if (source && userEqFilters && hiddenEqFilters && audioContext) {
+      const firstHiddenFilter = hiddenEqFilters[0];
+      source.connect(firstHiddenFilter);
 
-      filters[filters.length - 1].connect(audioContextRef.current.destination);
+      const lastHiddenFilter = hiddenEqFilters[hiddenEqFilters.length - 1];
+      const firstUserFilter = userEqFilters[0];
+      lastHiddenFilter.connect(firstUserFilter);
+
+      const lastUserFilter = userEqFilters[userEqFilters.length - 1];
+      const destination = audioContext.destination;
+      lastUserFilter.connect(destination);
+
+      return () => {
+        source.disconnect();
+        lastHiddenFilter.disconnect();
+        lastUserFilter.disconnect();
+      }
     }
-  };
+  }, [source, userEqFilters, hiddenEqFilters, audioContext]);
 
   const modEqSetting = (index: number, mod: (f: EQBand) => EQBand) => {
     setMixerSettings(prev => ({
@@ -83,15 +134,42 @@ function App() {
 
   // Sync change from eqSettings to the biquad filters
   useEffect(() => {
-    if (eqFilters) {
+    if (userEqFilters) {
       mixerSettings.parametricEq.forEach((band, index) => {
-        const filter = eqFilters[index];
+        const filter = userEqFilters[index];
         filter.frequency.value = band.frequency;
         filter.Q.value = band.q;
         filter.gain.value = band.gain;
       });
     }
-  }, [eqFilters, mixerSettings]);
+  }, [userEqFilters, mixerSettings]);
+
+  const scrambleHiddenEq = () => {
+    if (hiddenEqFilters) {
+      hiddenEqFilters.forEach((filter) => {
+        // Log compensate frequency
+        filter.frequency.value = Math.pow(10, Math.random() * 3) * 20; 
+        filter.Q.value = Math.random() * 10;
+
+        filter.gain.value = (-Math.random()) * 20;
+        // Generate random gain evenly spaced in audible range
+        // Math.pow(10, gain / 40);
+        //filter.gain.value = Math.pow(10, (-Math.random()) * 80 / 40) * 24;
+
+        console.log(filter.frequency.value, filter.Q.value, filter.gain.value);
+      });
+    }
+  }
+
+  const resetHiddenEq = () => {
+    if (hiddenEqFilters) {
+      hiddenEqFilters.forEach((filter) => {
+        filter.frequency.value = 100;
+        filter.Q.value = 1;
+        filter.gain.value = 0;
+      });
+    }
+  }
 
   return (
     <div>
@@ -107,12 +185,15 @@ function App() {
         </select> or {' '}
         <input type="file" accept="audio/*" onChange={handleFileChange} />
       </div>
-        <div>
-          {/* If the current audio source matches one of the known sources, display it's description */}
-          {audioTracks.find(track => track.src === audioElement?.src)?.description}
-          Test
-        </div>
+      <div style={{opacity: 0.5, fontSize:"0.8rem"}}>
+        {/* If the current audio source matches one of the known sources, display it's description */}
+        {audioTracks.find(track => audioElement?.src.endsWith(track.src))?.description}
+      </div>
       <audio ref={setAudioElement} controls onPlay={initAudioContext} />
+
+      <h1>Game</h1>
+      <button onClick={scrambleHiddenEq} >Scramble EQ</button>
+      <button onClick={resetHiddenEq}>Reset EQ</button>
 
 
       <h1>Parametric EQ Controls</h1>
@@ -122,73 +203,73 @@ function App() {
         <tbody>
           <tr>
             {mixerSettings.parametricEq.map((band, index) => (
-              <td key={`freq-${index}`}  style={{padding: '0.5em'}}>
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                <EQControl
-                  value={band.frequency}
-                  min={60}
-                  max={20000}
-                  logScale={true}
-                  onChange={(newVal) => modEqSetting(index, (prev) => ({
-                    ...prev,
-                    frequency: newVal
-                  }))}
-                />
-                Freq
+              <td key={`freq-${index}`} style={{ padding: '0.5em' }}>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                  <EQControl
+                    value={band.frequency}
+                    min={60}
+                    max={20000}
+                    logScale={true}
+                    onChange={(newVal) => modEqSetting(index, (prev) => ({
+                      ...prev,
+                      frequency: newVal
+                    }))}
+                  />
+                  Freq
                 </div>
               </td>
             ))}
           </tr>
           <tr>
             {mixerSettings.parametricEq.map((band, index) => (
-              <td key={`q-${index}`} style={{padding: '0.5em'}}>
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                <EQControl
-                  value={band.q}
-                  min={0.1}
-                  max={10}
-                  onChange={(newVal) => modEqSetting(index, (prev) => ({
-                    ...prev,
-                    q: newVal
-                  }))}
-                />
-                Width
+              <td key={`q-${index}`} style={{ padding: '0.5em' }}>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                  <EQControl
+                    value={band.q}
+                    min={0.1}
+                    max={10}
+                    onChange={(newVal) => modEqSetting(index, (prev) => ({
+                      ...prev,
+                      q: newVal
+                    }))}
+                  />
+                  Width
                 </div>
               </td>
             ))}
           </tr>
           <tr>
             {mixerSettings.parametricEq.map((band, index) => (
-              <td key={`gain-${index}`} style={{padding: '0.5em'}}> 
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                <EQControl
-                  value={band.gain}
-                  min={-18}
-                  max={18}
-                  onChange={(newVal) => modEqSetting(index, (prev) => ({
-                    ...prev,
-                    gain: newVal
-                  }))}
-                />
-                Gain
+              <td key={`gain-${index}`} style={{ padding: '0.5em' }}>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                  <EQControl
+                    value={band.gain}
+                    min={-18}
+                    max={18}
+                    onChange={(newVal) => modEqSetting(index, (prev) => ({
+                      ...prev,
+                      gain: newVal
+                    }))}
+                  />
+                  Gain
                 </div>
               </td>
             ))}
           </tr>
           <tr>
             {mixerSettings.parametricEq.map((band, index) => (
-                <td key={`name-${index}`} style={{padding: '0.5em'}}>
-                  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', fontWeight: 'bold' }}>
-                    {band.name}
-                  </div>
-                </td>
+              <td key={`name-${index}`} style={{ padding: '0.5em' }}>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', fontWeight: 'bold' }}>
+                  {band.name}
+                </div>
+              </td>
             ))}
           </tr>
         </tbody>
       </table>
       <EQView
         bands={mixerSettings.parametricEq}
-        />
+      />
     </div>
   );
 }
