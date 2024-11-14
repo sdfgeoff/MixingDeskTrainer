47828c7e0988d25511ab6de9a767aa353d050b01
Geoffrey Irons <sdfgeoff@gmail.com>
2024-11-15 11:46:30 +1300

    Split out some files, added linting check

---
+++ src/hooks/useAudioDestination.ts
@@ -0,0 +1,16 @@
+import { useEffect } from "react"
+
+export const useAudioDestination = (audioContext: AudioContext | undefined, listenTo: AudioNode | undefined) => {
+    // Connect the listenTo node to the audioContext destination
+    useEffect(() => {
+      if (audioContext && listenTo) {
+        listenTo.connect(audioContext.destination)
+      }
+      return () => {
+        if (audioContext && listenTo) {
+          listenTo.disconnect(audioContext.destination)
+        }
+      }
+    }, [audioContext, listenTo])
+  }
+  
---
+++ src/hooks/useAudioLevel.ts
@@ -0,0 +1,51 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+/**
+ * A hook that listens to an audio node and returns the output level in dB
+ */
+export const useAudioLevel = (audioContext: AudioContext | undefined, listenTo: AudioNode | undefined): number => {
+    const [outputDb, setOutputDb] = useState<number>(-40);
+  
+    const outputAnalyzerNode = useMemo(() => {
+      if (!audioContext) {
+        return undefined
+      }
+      const analyzerNode = audioContext.createAnalyser();
+      analyzerNode.fftSize = 256;
+      return analyzerNode
+    }, [audioContext])
+  
+    const updateOutputAudioLevel = useCallback(() => {
+      requestAnimationFrame(updateOutputAudioLevel) // TODO: figure out when to stop this!
+  
+      const dataArray = new Float32Array(outputAnalyzerNode?.frequencyBinCount ?? 0);
+  
+      outputAnalyzerNode?.getFloatTimeDomainData(dataArray)
+      // Get RMS
+      const rms = Math.sqrt(dataArray.reduce((acc, val) => acc + val * val, 0) / dataArray.length);
+      // Convert to dB
+  
+      const db = 20 * Math.log10(rms * 12); // The *12 is so the output level hits peaks at +12 DB
+      setOutputDb(db)
+    }, [setOutputDb, outputAnalyzerNode])
+  
+    useEffect(() => {
+      if (outputAnalyzerNode) {
+        updateOutputAudioLevel()
+      }
+    }, [outputAnalyzerNode, updateOutputAudioLevel])
+  
+    useEffect(() => {
+      if (!outputAnalyzerNode || !listenTo) {
+        return () => { }
+      }
+      listenTo.connect(outputAnalyzerNode)
+  
+      return () => {
+        listenTo.disconnect(outputAnalyzerNode)
+      }
+  
+    }, [listenTo, outputAnalyzerNode])
+  
+    return outputDb
+  }
