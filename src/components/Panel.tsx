d36bfa0c56f184e70a74df27411bd49124204b0f
Geoffrey Irons <sdfgeoff@gmail.com>
2025-01-28 00:46:14 +1300

    Make panels a bit smaller

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -9,9 +9,9 @@ export interface PanelProps {
 
 export const Panel: React.FC<PanelProps> = ({ children, heading, color = COLORS.background_colorful }) => {
     return (
-        <div style={{ borderRadius: BORDER_RADIUS, background: color, padding: PADDING.small, display: 'flex', flexDirection: 'column', gap: PADDING.small }}>
+        <div style={{ borderRadius: BORDER_RADIUS, background: color, padding: '0.3rem', display: 'flex', flexDirection: 'column', gap: PADDING.small }}>
             {heading && <div style={{ textAlign: 'center', fontWeight: 'bold' }}>{heading}</div>}
-            <div style={{ border: `0.2rem solid ${COLORS.primary}`, borderRadius: BORDER_RADIUS, background: COLORS.background, padding: PADDING.medium }}>
+            <div style={{ border: `0.1rem solid ${COLORS.primary}`, borderRadius: BORDER_RADIUS, background: COLORS.background, padding: PADDING.medium }}>
                 {children}
             </div>
         </div>
@@ -25,14 +25,14 @@ export interface MinimizablePanelProps extends PanelProps {
 export const MinimizablePanel: React.FC<MinimizablePanelProps> = ({ children, heading, color = COLORS.background_colorful, startExpanded }) => {
     const [expanded, setExpanded] = React.useState(startExpanded)
     return (
-        <div style={{ borderRadius: BORDER_RADIUS, background: color, padding: PADDING.small, display: 'flex', flexDirection: 'column', gap: PADDING.small }}>
+        <div style={{ borderRadius: BORDER_RADIUS, background: color, padding: '0.3rem', display: 'flex', flexDirection: 'column', gap: PADDING.small }}>
             <div onClick={() => setExpanded((prev) => !prev)} style={{cursor: 'pointer'}}>
                 <div style={{ display: 'flex', alignItems: 'center', paddingLeft: PADDING.small, paddingRight: PADDING.small }}>
                     <div style={{ textAlign: 'center', fontWeight: 'bold', flexGrow: 1 }}>{heading}</div>
                     <div style={{fontSize: FONTSIZE.small, textDecoration: 'underline'}}>{expanded ? 'minimize' : 'expand'}</div>
                 </div>
             </div>
-            {<div style={{ border: `0.2rem solid ${COLORS.primary}`, borderRadius: BORDER_RADIUS, background: COLORS.background, padding: PADDING.medium, display: expanded ? 'block' : 'none' }}>
+            {<div style={{ border: `0.1rem solid ${COLORS.primary}`, borderRadius: BORDER_RADIUS, background: COLORS.background, padding: PADDING.medium, display: expanded ? 'block' : 'none' }}>
                 {children}
             </div>}
         </div>
